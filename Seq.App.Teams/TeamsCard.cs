7152acf1f1b786c8e3de212c007b70d07cd08bc0
Antoine <gay.antoine@gmail.com>
2016-11-08 13:24:06 +0100

    Added serialization name

--- Seq.App.Teams/TeamsCard.cs
+++ Seq.App.Teams/TeamsCard.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Seq.App.Teams
 {
     public class TeamsCard
     {
+        [JsonProperty(PropertyName ="title")]
         public string Title { get; set; }
+        [JsonProperty(PropertyName = "text")]
         public string Text { get; set; }
+        [JsonProperty(PropertyName = "themeColor")]
         public string ThemeColor { get; set; } = "black";
+        [JsonProperty(PropertyName = "potentialAction")]
         public TeamsPotentialAction[] PotentialAction { get; set; }
     }
 }
--- Seq.App.Teams/TeamsPotentialAction.cs
+++ Seq.App.Teams/TeamsPotentialAction.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Seq.App.Teams
 {
     public class TeamsPotentialAction
     {
-
+        [JsonProperty(PropertyName = "@context")]
         public string Context { get; set; } = "https://schema.org";
+        [JsonProperty(PropertyName = "@type")]
         public string Type { get; set; } = "ViewAction";
+        [JsonProperty(PropertyName = "name")]
         public string Name { get; set; }
+        [JsonProperty(PropertyName = "target")]
         public string Target { get; set; }
     }
 }
